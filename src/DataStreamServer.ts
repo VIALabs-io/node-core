4d70a7020d583408b3407b63a5bb078adaf4d72d
AtlasCLT <atlas@cryptolink.tech>
2024-04-30 17:57:22 +0200

    open cors

--- src/DataStreamServer.ts
+++ src/DataStreamServer.ts
@@ -17,6 +17,13 @@ class DataStreamServer {
         this.server = http.createServer(this.app);
         this.io = new SocketIOServer(this.server);
         this.port = port;
+        
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            next();
+        });
 
         this.configureRoutes();
         this.handleSocketConnections();
